43bc82e7c6ee2dc0764fbaf25d49074be752ee3b
allycia <allyciarocha@ufpi.edu.br>
2025-10-20 22:06:52 -0300

    criando layout das primeiras paginas com bootstrap e html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './_components/navbar/navbar';
+import { CommonModule } from '@angular/common';
+import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+import { BaseUi } from "./_components/base-ui/base-ui";
+import { Certificados } from "./pages/certificados/certificados";
+import { CertificadoForm } from "./pages/certificado-form/certificado-form";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Navbar,
+    CommonModule,
+    PrimaryButton,
+    SecondaryButton,
+    ItemCertificado,
+    BaseUi,
+    Certificados,
+    CertificadoForm
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'gerador-certificado';
+  exibeNavbar: boolean = true;
 }
