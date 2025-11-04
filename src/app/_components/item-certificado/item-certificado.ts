cbf5d9ed9d0c7d512dfd2b316d751b68381ff292
allycia <allyciarocha@ufpi.edu.br>
2025-11-03 21:11:36 -0300

    feat: ajustes no layout e uso do ngModel, ngStyle e ngClass

--- src/app/_components/item-certificado/item-certificado.ts
+++ src/app/_components/item-certificado/item-certificado.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../secondary-button/secondary-button";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, RouterLink],
   templateUrl: './item-certificado.html',
   styleUrl: './item-certificado.css'
 })
 export class ItemCertificado {
+  id: string = '6';
+  //chamado assim que o componente é instanciado
+  constructor(
+    private router: Router
+  ){}
 
+  /*inicializado depois que o angulat inicializa o componente e o DOM
+  ngOnInit(): void {}*/
+
+  redirecionaCertificado(){
+    //
+    this.router.navigate(['/certificados', 2]); // certificados/2 - "[routerLink]"
+    //this.router.navigateByUrl('/certificados' + this.id); - certificados/2 - "routerLink"
+
+  }
 }
