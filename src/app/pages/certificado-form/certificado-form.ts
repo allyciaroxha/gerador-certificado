a2e10845fdd7d3e3193a79c414a702ff48f82f6b
allycia <allyciarocha@ufpi.edu.br>
2025-11-03 22:13:56 -0300

    feat: finaliza criacao dos formularios

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -3,6 +3,7 @@ import { PrimaryButton } from "../../_components/primary-button/primary-button";
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
 import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Certificado } from '../../interface/certificado';
 
 @Component({
   selector: 'app-certificado-form',
@@ -12,15 +13,34 @@ import { CommonModule } from '@angular/common';
 })
 export class CertificadoForm {
 
-  nome: string = '';
+  certificado: Certificado = {
+    atividades: [],
+    nome: ''
+  }
+
   atividade: string = '';
-  atividades: string[] = ['Angular', 'React', 'Cobol'];
 
   campoInvalido(control: NgModel) {
     return control.invalid && control.touched
   }
 
   formValido() {
-    return this.atividades.length>0 && this.nome.length>0;
+    return this.certificado.atividades.length>0 && this.certificado.nome.length>0;
+  }
+
+  adicionarAtividade() {
+    this.certificado.atividades.push(this.atividade);
+    this.atividade = '';
+  }
+
+  excluirAtividade(index: number){
+    this.certificado.atividades.splice(index, 1);
+  }
+
+  submit(){
+    if(!this.formValido()){
+      return;
+    }
+    console.log(this.certificado);
   }
 }
